1dc73127a460eb10bb219da222ef05e67b67c45f
Jose Noguera <nogueraruiz@gmail.com>
2021-01-26 15:27:22 -0300

    Initial commit

---
+++ ios/Pods/Target Support Files/Pods-QuantoRNTest-QuantoRNTestTests/Pods-QuantoRNTest-QuantoRNTestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QuantoRNTest_QuantoRNTestTests : NSObject
+@end
+@implementation PodsDummy_Pods_QuantoRNTest_QuantoRNTestTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-QuantoRNTest-tvOS/Pods-QuantoRNTest-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QuantoRNTest_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_QuantoRNTest_tvOS
+@end
